6b13b3b7adb5a087290503374baeae3365306743
iif <hasbiasshidiq@gmail.com>
2021-07-26 07:24:03 +0700

    make cleaner directory

--- auth-server/main.go
+++ auth-server/main.go
@@ -4,16 +4,23 @@ import (
 	"auth-server/token"
 	"log"
 	"net"
+	"os"
 
 	auth_pb "github.com/hasbiasshidiq/auth-stub-5"
 
+	"github.com/joho/godotenv"
 	"google.golang.org/grpc"
 	"google.golang.org/grpc/reflection"
 )
 
 func main() {
 
-	port := "50059"
+	if err := godotenv.Load(); err != nil {
+		log.Println("Error loading .env file")
+		log.Println("using default environment variable")
+	}
+
+	port := os.Getenv("API_PORT")
 
 	l, err := net.Listen("tcp", ":"+port)
 	if err != nil {
